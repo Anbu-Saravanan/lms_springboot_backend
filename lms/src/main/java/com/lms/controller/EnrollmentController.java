739fe443224d786a7f2dc7b8a1b2afabd06c8014
Anbu-Saravanan <anbu.saravanan11211@gmail.com>
2025-08-02 10:36:11 +0530

    Update EnrollmentController.java

--- lms/src/main/java/com/lms/controller/EnrollmentController.java
+++ lms/src/main/java/com/lms/controller/EnrollmentController.java
@@ -16,7 +16,7 @@ import java.security.Principal;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/courses/enrollments")
+@RequestMapping("/api/courses/{courseId}/enrollments")
 public class EnrollmentController {
 
     @Autowired
